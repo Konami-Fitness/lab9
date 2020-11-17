30163653cc84305065e483c6a8a031a4dc1a1b30
mkokkatt <47791277+mkokkatt@users.noreply.github.com>
2020-11-16 23:13:10 -0500

    Update lab9.php

--- lab9.php
+++ lab9.php
@@ -6,7 +6,7 @@ $password = "itws";
 
 // Create connection
 try {
-  $dbconn = new PDO('mysql:host=localhost;dbname=test2',$username,$password);
+  $dbconn = new PDO('mysql:host=localhost;dbname=websyslab9',$username,$password);
   $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
 } catch (PDOException $e){
@@ -14,14 +14,6 @@ try {
 }
 
 
-// $dbconn = new mysqli($servername, $username, $password);
-// Check connection
-
-// if ($dbconn->connect_error) {
-//   die("Connection failed: " . $dbconn->connect_error);
-// }
-
-// Create database
 $sql = "CREATE TABLE grades(id int AUTO_INCREMENT, crn int, rin int, grade int(3) NOT NULL,
     PRIMARY KEY(id), FOREIGN KEY (crn) REFERENCES courses(crn) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (rin) REFERENCES students(rin) ON DELETE CASCADE ON UPDATE CASCADE)";
@@ -32,72 +24,52 @@ if (($result = $dbconn->query($sql)) !== FALSE) {
   echo "Error creating table: ";
 }
 
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $tn = $_POST['tablename'];
+    $cn = $_POST['columnname'];
+    $ct = $_POST['columntype'];
+    $nn = $_POST['notnull'];
+    $ai = $_POST['auto-increment'];
 
-// interface operations {
-//   public function operate();
-//   public function getEquation();
-// }
-//
-// abstract class Operation1 {
-//   protected $operand_1;
-//   public function __construct($o1) {
-// 		if (strcmp($o1, 'pi') == 0) {
-// 			$o1 = pi();
-// 		} else if (strcmp($o1, 'e') == 0) {
-// 			$o1 = exp(1);
-// 		}
-//     if (!is_numeric($o1)) {
-//       throw new Exception('Non-numeric operand.');
-//     }
-//     $this->operand_1 = $o1;
-//   }
-// }
-//
-// class Square extends Operation1 implements operations {
-//   public function operate() {
-//       return pow($this->operand_1, 2);
-//   }
-//   public function getEquation() {
-//     return $this->operand_1 . '^2 = ' . $this->operate();
-//   }
-// }
-
-
-
-//Some debugs - uncomment these to see what is happening...
-// echo '$_POST print_r=>',print_r($_POST);
-// echo "<br>",'$_POST vardump=>',var_dump($_POST);
-// echo '<br/>$_POST is ', (isset($_POST) ? 'set' : 'NOT set'), "<br/>";
-// echo "<br/>---";
-
-
-// Check to make sure that POST was received
-// upon initial load, the page will be sent back via the initial GET at which time
-// the $_POST array will not have values - trying to access it will give undefined message
-
-  if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $o1 = $_POST['op1'];
-    $o2 = $_POST['op2'];
   }
   $err = Array();
 
+  if($nn == 'y') {
+    $nn = 'NOT NULL';
+  } else {
+    $nn = '';
+  }
 
-// Instantiate an object for each operation based on the values returned on the form
-// For example, check to make sure that $_POST is set and then check its value and
-// instantiate its object
-//
-// The Add is done below.  Go ahead and finish the remiannig functions.
-// Then tell me if there is a way to do this without the ifs
-// We might cover such a way on Tuesday...
+  if($ai == 'y') {
+    $ai = 'AUTO_INCREMENT';
+  } else {
+    $ai = '';
+  }
 
-  try {
-    if (isset($_POST['add']) && $_POST['add'] == 'Add') {
-      $op = new Addition($o1, $o2);
+  $sql2 = 'ALTER TABLE ' . $tn . $cn . $nn . $an;
+
+//ex2
+ 
+  $q2 = $dbconn->query($sql2);
+
+
+
+  $result = $q2->fetchAll()[0]['grade'];
+  
+/*
+ try {
+    if (isset($_POST['altertable']) && $_POST['altertable'] == 'Add') {
+      alterTable();
     }
   }
   catch (Exception $e) {
     $err[] = $e->getMessage();
   }
+*/
+
+//ex3
+ // $sql3 = 'DELETE FROM customers WHERE id = 4';
+  //$customers1 = $dbconn->query($sql3);
 ?>
 
 <!doctype html>
@@ -113,55 +85,28 @@ if (($result = $dbconn->query($sql)) !== FALSE) {
     <div class="calcbox">
       <pre id="result">
   	  <?php
-  	    if (isset($op)) {
-  	      try {
-  	        echo $op->getEquation();
-  	      }
-  	      catch (Exception $e) {
-  	        $err[] = $e->getMessage();
-  	      }
-  	    }
-
-  	    foreach($err as $error) {
-  	        echo $error . "\n";
-  	    }
+  	    if (isset($sql)) {
+          try {
+            echo $sql;
+          }
+          catch (Exception $e) {
+            $err[] = $e->getMessage();
+          }
+        }
+         foreach($err as $error) {
+            echo $error . "\n";
+        }
   	  ?>
   	  </pre>
       <br>
-  	  <form method="post" action="lab9.php">
-  	    <input type="text" name="op1" id="name" value="" />
-  	    <input type="text" name="op2" id="name" value="" />
-  	    <br/>
-  	    <!-- Only one of these will be set with their respective value at a time -->
-  			<div class="left">
-          <p>1 Input</p>
-  				<div class="leftrow">
-  					<input type="submit" name="add" value="Add" />
-  					<input type="submit" name="sub" value="Subtract" />
-  					<input type="submit" name="mult" value="Multiply" />
-  				</div>
-  				<div class="leftrow">
-  					<input type="submit" name="divi" value="Divide" />
-  					<input type="submit" name="expo" value="Exponent" />
-            <input type="submit" name="square" value="Square" />
-  				</div>
-  			</div>
-  			<div class="right">
-          <p>2 Inputs</p>
-  				<div class="rightrow">
-  					<input type="submit" name="sqrt" value="Sqrt" />
-  					<input type="submit" name="log10" value="Log10" />
-            <input type="submit" name="ln" value="Ln" />
-  				</div>
-  				<div class="rightrow">
-  					<input type="submit" name="tenexp" value="10^x" />
-  					<input type="submit" name="eexp" value="e^x" />
-            <input type="submit" name="sin" value="Sin" />
-  				</div>
-  				<div class="rightrow">
-  					<input type="submit" name="cos" value="Cos" />
-  					<input type="submit" name="tan" value="Tan" />
-  				</div>
+  	  <form method="post" action="main.php">
+  	    <input type="text" name="tablename" id="name" value="" />
+  	    <input type="text" name="columnname" id="name" value="" />
+        <input type="text" name="columntype" id="name" value="" />
+        <input type="text" name="notnull" id="name" value="" />
+        <input type="text" name="auto-inc" id="name" value="" />
+
+        <input type="submit" name="addCol" value="add column" />	
   			</div>
   	  </form>
     </div>
