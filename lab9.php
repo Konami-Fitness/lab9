5868ce8f4e446a6107f8aa228244accd2b7b3306
mkokkatt <47791277+mkokkatt@users.noreply.github.com>
2020-11-17 00:08:43 -0500

    Add files via upload

--- lab9.php
+++ lab9.php
@@ -1,71 +1,37 @@
+
+
+
 <?php
+  $dbconn = new PDO('mysql:host=localhost;dbname=websyslab9','user','itws');
 
-$servername = "localhost";
-$username = "user";
-$password = "itws";
-
-// Create connection
-try {
-  $dbconn = new PDO('mysql:host=localhost;dbname=websyslab9',$username,$password);
-  $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch (PDOException $e){
-  echo "Connection failed: " . $e->getMessage();
-}
-
-
-$sql = "CREATE TABLE grades(id int AUTO_INCREMENT, crn int, rin int, grade int(3) NOT NULL,
-    PRIMARY KEY(id), FOREIGN KEY (crn) REFERENCES courses(crn) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (rin) REFERENCES students(rin) ON DELETE CASCADE ON UPDATE CASCADE)";
-
-if (($result = $dbconn->query($sql)) !== FALSE) {
-  echo "Table created successfully";
-} else {
-  echo "Error creating table: ";
-}
-
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $tn = $_POST['tablename'];
-    $cn = $_POST['columnname'];
-    $ct = $_POST['columntype'];
-    $nn = $_POST['notnull'];
-    $ai = $_POST['auto-increment'];
-
-  }
-  $err = Array();
-
-  if($nn == 'y') {
-    $nn = 'NOT NULL';
-  } else {
-    $nn = '';
+  try {
+  if(isset($dbconn)) {
+    echo 'Connected!';
   }
 
-  if($ai == 'y') {
-    $ai = 'AUTO_INCREMENT';
-  } else {
-    $ai = '';
-  }
-
-  $sql2 = 'ALTER TABLE ' . $tn . $cn . $nn . $an;
-
-//ex2
-
-  $q2 = $dbconn->query($sql2);
-
-
-
-  $result = $q2->fetchAll()[0]['grade'];
-
-/*
- try {
-    if (isset($_POST['altertable']) && $_POST['altertable'] == 'Add') {
-      alterTable();
-    }
   }
   catch (Exception $e) {
     $err[] = $e->getMessage();
   }
+/*
+  //ex1
+  $sql = 'ALTER TABLE :table ADD' formInput.columnname, ect
+  $q1 = $dbconn->prepare($sql);
+  $q1->execute(array(':table' => formInput.table));
+
 */
+
+//ex2
+  $sql2 = 'SELECT * FROM grades WHERE id = :id';
+  $q2 = $dbconn->prepare($sql2);
+  $q2->execute(array(':id' => 1));
+
+
+  $result = $q2->fetchAll()[0]['grade'];
+  
+
+ 
+
 
 //ex3
  // $sql3 = 'DELETE FROM customers WHERE id = 4';
@@ -74,66 +40,34 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <!doctype html>
 <html>
-	<head>
-		<title>Konami Grade Book</title>
-		<link rel=stylesheet href="lab9.css"/>
+  <head>
+    <title>Konami Calculator</title>
+    <link rel=stylesheet href="calculator.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Raleway&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap" rel="stylesheet">
-	</head>
-	<body>
-		<h1>Konami Grade Book</h1>
-    <div class="calcbox">
+  </head>
+  <body>
+    <h1>Konami Calculator</h1>
+
+
+
+
+
       <pre id="result">
-  	  <?php
-  	    if (isset($sql)) {
-          try {
-            echo $sql;
-          }
-          catch (Exception $e) {
-            $err[] = $e->getMessage();
-          }
-        }
-         foreach($err as $error) {
-            echo $error . "\n";
-        }
-  	  ?>
-  	  </pre>
-      <br>
-  	  <form method="post" action="main.php">
-  	    <input type="text" name="tablename" id="name" value="" />
-  	    <input type="text" name="columnname" id="name" value="" />
-        <input type="text" name="columntype" id="name" value="" />
-        <input type="text" name="notnull" id="name" value="" />
-        <input type="text" name="auto-inc" id="name" value="" />
-
-        <input type="submit" name="addCol" value="add column" />
-  			</div>
-  	  </form>
-      
-      <form name="insertstudent" method="post" action="lab9.php">
-        <input type="number" name="rin" value="">
-        <input type="text" name="rcsid" value="">
-        <input type="text" name="fname" value="">
-        <input type="text" name="lname" value="">
-        <input type="text" name="alias" value="">
-        <input type="number" name="phone" value="">
-        <input type="text" name="street" value="">
-        <input type="text" name="city" value="">
-        <input type="text" name="state" value="">
-        <input type="text" name="zip" value="">
-
-        <input type="submit" name="insertStudent" value="insert student">
+  
+      </pre>
+      <form method="post" action="lab9.php">
+        <input type="text" name="op1" id="name" value="" />
+        <input type="text" name="op2" id="name" value="" />
+        <br/>       
+        <input type="submit" name="add" value="Add" />
       </form>
+    <div id = "results">hi</div>
 
-      <form name="insertgrade" method="post" action="lab9.php">
-        <input type="number" name="id" value="">
-        <input type="text" name="crn" value="">
-        <input type="number" name="rin" value="">
-        <input type="number" name="grade" value="">
-
-        <input type="submit" name="insertGrade" value="insert grade">
-      </form>
-
-    </div>
-	</body>
+    <script type="text/javascript"> 
+      var x =  "<?php echo"$result"?>";
+      document.getElementById("results").innerHTML = x;
+   </script>
+  </body>
 </html>
+
