68c73d02c47442cf1c87a2cd04482e9009bfe253
mkokkatt <47791277+mkokkatt@users.noreply.github.com>
2020-11-17 00:21:04 -0500

    Update lab9.php

--- lab9.php
+++ lab9.php
@@ -1,36 +1,79 @@
-
-
-
 <?php
-  $dbconn = new PDO('mysql:host=localhost;dbname=websyslab9','user','itws');
 
-  try {
-  if(isset($dbconn)) {
-    echo 'Connected!';
+$servername = "localhost";
+$username = "user";
+$password = "itws";
+
+// Create connection
+try {
+  $dbconn = new PDO('mysql:host=localhost;dbname=websyslab9',$username,$password);
+  $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch (PDOException $e){
+  echo "Connection failed: " . $e->getMessage();
+}
+
+/*
+$sql = "CREATE TABLE grades(id int AUTO_INCREMENT, crn int, rin int, grade int(3) NOT NULL,
+    PRIMARY KEY(id), FOREIGN KEY (crn) REFERENCES courses(crn) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (rin) REFERENCES students(rin) ON DELETE CASCADE ON UPDATE CASCADE)";
+
+if (($result = $dbconn->query($sql)) !== FALSE) {
+  echo "Table created successfully";
+} else {
+  echo "Error creating table: ";
+}*/
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $tn = $_POST['tablename'];
+    $cn = $_POST['columnname'];
+    $ct = $_POST['columntype'];
+    $nn = $_POST['notnull'];
+    $ai = $_POST['auto-inc'];
+
   }
+    $err = Array();
+
+
+function alterTable($tn,$cn,$ct,$nn,$ai, $dbconn) {
+ if($nn == 'y') {
+    $nn = 'NOT NULL';
+  } else {
+    $nn = '';
+  }
+
+  if($ai == 'y') {
+    $ai = 'AUTO_INCREMENT';
+  } else {
+    $ai = '';
+  }
+  echo $tn;
+  echo $nn;
+  echo $ai;
+  $sql2 = 'ALTER TABLE ' . $tn . ' ADD '. $cn . ' '. $ct . ' ' . $nn . ' '. $ai;
+
+//ex2
+ 
+  $q2 = $dbconn->query($sql2);
+
+
+  }
+
+
+
+
+  
+
+ try {
+    if (isset($_POST['addCol']) && $_POST['addCol'] == 'add column') {
+      alterTable($tn,$cn,$ct,$nn,$ai, $dbconn);
+    }
+
 
   }
   catch (Exception $e) {
     $err[] = $e->getMessage();
   }
-/*
-  //ex1
-  $sql = 'ALTER TABLE :table ADD' formInput.columnname, ect
-  $q1 = $dbconn->prepare($sql);
-  $q1->execute(array(':table' => formInput.table));
-
-*/
-
-//ex2
-  $sql2 = 'SELECT * FROM grades WHERE id = :id';
-  $q2 = $dbconn->prepare($sql2);
-  $q2->execute(array(':id' => 1));
-
-
-  $result = $q2->fetchAll()[0]['grade'];
-  
-
- 
 
 
 //ex3
@@ -40,34 +83,30 @@
 
 <!doctype html>
 <html>
-  <head>
-    <title>Konami Calculator</title>
-    <link rel=stylesheet href="calculator.css"/>
+	<head>
+		<title>Konami Grade Book</title>
+		<link rel=stylesheet href="lab9.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Raleway&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <h1>Konami Calculator</h1>
-
-
-
-
-
+	</head>
+	<body>
+		<h1>Konami Grade Book</h1>
+    <div class="calcbox">
       <pre id="result">
-  
-      </pre>
-      <form method="post" action="lab9.php">
-        <input type="text" name="op1" id="name" value="" />
-        <input type="text" name="op2" id="name" value="" />
-        <br/>       
-        <input type="submit" name="add" value="Add" />
-      </form>
-    <div id = "results">hi</div>
+  	  
+  	  
+  	  </pre>
+      <br>
+  	  <form method="post" action="main.php">
+  	    <input type="text" name="tablename" id="name" value="" />
+  	    <input type="text" name="columnname" id="name" value="" />
+        <input type="text" name="columntype" id="name" value="" />
+        <input type="text" name="notnull" id="name" value="" />
+        <input type="text" name="auto-inc" id="name" value="" />
 
-    <script type="text/javascript"> 
-      var x =  "<?php echo"$result"?>";
-      document.getElementById("results").innerHTML = x;
-   </script>
-  </body>
+        <input type="submit" name="addCol" value="add column" />	
+  			</div>
+  	  </form>
+    </div>
+	</body>
 </html>
-
